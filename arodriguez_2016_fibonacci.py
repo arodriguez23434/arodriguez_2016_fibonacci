2338a1a6225ecb5e3fe0a311c1bc4f40ef507ed2
arodriguez23434 <arodriguez23434@knights.ucf.edu>
2016-01-26 00:42:47 -0500

    Replaced usage of long integers with single list
    Rather than storing several integers in long long form as individual
    variables, I instead opted to update the code to use only a single list
    and refer to previous indices when making calculations. This simplifies
    the process and removes visual clutter.

--- arodriguez_2016_fibonacci.py
+++ arodriguez_2016_fibonacci.py
@@ -1,15 +1,10 @@
+#!/usr/bin/env python
 def fibo(int):
-    a=0;b=0;sum=0;fibo_list=list()
-    for i in range(0,int+2):
-        #Indices 0 & 1 are pre-set; Addition begins at index 2
-        if i==1: 
-            sum=1;
-        #Add previous sum to previous largest integer: (a+b)=sum -> (b+sum)=new_sum -> ...
-        else:
-            a=b; 
-            b=sum;
-            sum=a+b; 
-        fibo_list.append(sum) #List used instead of printing every iteration for modularity
-    return fibo_list
+    #Indices 0 & 1 are pre-set; Addition begins at index 2
+    fibo_list = [0,1];
+    for i in range(2,int):
+        #Add previous sum to previous largest integer: (a+b)=sum -> (b+sum)=new_sum -> ... then append to list
+        fibo_list.append(fibo_list[i-2]+fibo_list[i-1]);
+    return fibo_list;
 
-print(fibo(100)) #Print list 
+print(fibo(100)); #Print list 
